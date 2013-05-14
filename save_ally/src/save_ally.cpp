29691989242e469ece9f2e60722501792f8ad0cb
rainloader <rainloader@gmail.com>
2013-05-14 17:40:25 +0900

    <save_ally> solved.
    But not compact.

--- save_ally/src/save_ally.cpp
+++ save_ally/src/save_ally.cpp
@@ -7,10 +7,10 @@
 //============================================================================
 
 #include <iostream>
-#include <vector>
 using namespace std;
 
-int save_ally(int numOfMember);
+int save_ally(int iNumOfMember);
+bool is_appropriate_interval(int iNumOfMember, int iInterval);
 
 int main() {
 
@@ -23,13 +23,10 @@ int main() {
 
 
 
-
-
-
-
-	//int elementNum = 6;
-	//std::vector<bool> lineup (elementNum);
-	char lineupArray[6] = {'a', 'a', 'a', 'e', 'e', 'e'};
+	for(int i=1; i<=13; i++){
+		cout << i << " " << save_ally(i) << endl;
+	}
+	//cout << save_ally(3) << endl;
 
 
 
@@ -47,16 +44,54 @@ int main() {
 
 
 
-int save_ally(int numOfMember){
-	int* lineUpArray = new int[numOfMember];
+int save_ally(int iNumOfMember){
 
+	// test from interval 1
+	for(int iInterval=iNumOfMember+1; /*iInterval<=(iNumOfMember*2)*/; iInterval++){
+		if(is_appropriate_interval(iNumOfMember, iInterval)){
+			return iInterval;
+		}
+	}
 
-	delete [] lineUpArray;
-	return 0;
+	// if there`s no answer
+	return -1;
 }
 
+bool is_appropriate_interval(int iNumOfMember, int iInterval){
+	char* pcLineUpArray = new char[iNumOfMember*2];
+	for(int i=0; i<iNumOfMember; i++){
+		pcLineUpArray[i] = 'a';
+		pcLineUpArray[iNumOfMember+i] = 'e';
+	}
 
-
+	int iDisplace = 0;
+	int i = -1;
+	int iCountKillingEnemy = 0;
+	while(iCountKillingEnemy != iNumOfMember){
+		iDisplace++;
+		//skip dead person. only admit alive person.
+		do{
+			i = (i+1)%(iNumOfMember*2);
+		}
+		while(pcLineUpArray[i] == 'd');
+		//when moved interval
+		if(iDisplace == iInterval){
+			//if target is ally, it is wrong interval.
+			 if(pcLineUpArray[i] == 'a'){
+				delete [] pcLineUpArray;
+				return false;
+			 }
+			 else{
+				//else, kill enemy and count killing enemy.
+				pcLineUpArray[i] = 'd';
+				iCountKillingEnemy++;
+				iDisplace = 0;
+			 }
+		}
+	}
+	delete [] pcLineUpArray;
+	return true;
+}
 
 
 
