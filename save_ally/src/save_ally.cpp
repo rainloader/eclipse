72f1fe5e0600d15311cd0deff382b3bc64257bfc
rainloader <rainloader@gmail.com>
2013-05-14 16:43:38 +0900

    <save_ally>
    algorithm quiz start

---
+++ save_ally/src/save_ally.cpp
@@ -0,0 +1,65 @@
+//============================================================================
+// Name        : save_ally.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int save_ally(int numOfMember);
+
+int main() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	//int elementNum = 6;
+	//std::vector<bool> lineup (elementNum);
+	char lineupArray[6] = {'a', 'a', 'a', 'e', 'e', 'e'};
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+int save_ally(int numOfMember){
+	int* lineUpArray = new int[numOfMember];
+
+
+	delete [] lineUpArray;
+	return 0;
+}
+
+
+
+
+
+
+
+
+
